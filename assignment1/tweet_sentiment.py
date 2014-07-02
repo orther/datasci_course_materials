d6a63621bba088f302ebfa32fd1244f1f669fb83
Brandon Orther <coder@able.cd>
2014-07-02 07:51:34 -0700

    Updated tweet_sentiment.py to working form.

--- assignment1/tweet_sentiment.py
+++ assignment1/tweet_sentiment.py
@@ -1,17 +1,35 @@
+import json
 import sys
 
-def hw():
-    print 'Hello, world!'
 
 def lines(fp):
     print str(len(fp.readlines()))
 
+
+def build_sentiment_scores(sentiment_file):
+    scores = {} # initialize an empty dictionary
+
+    with open(sentiment_file) as f:
+        for line in f:
+            term, score  = line.split("\t")  # The file is tab-delimited. "\t" means "tab character"
+            scores[term] = int(score)  # Convert the score to an integer.
+
+    return scores
+
+
 def main():
-    sent_file = open(sys.argv[1])
-    tweet_file = open(sys.argv[2])
-    hw()
-    lines(sent_file)
-    lines(tweet_file)
+    sentiment_scores = build_sentiment_scores(sys.argv[1])
+
+    with open(sys.argv[2]) as f:
+        for line in f:
+            score = 0
+            tweet = json.loads(line)
+
+            for term in tweet.get("text", "").split():
+                score += sentiment_scores.get(term, 0)
+
+            print score
+
 
 if __name__ == '__main__':
     main()
