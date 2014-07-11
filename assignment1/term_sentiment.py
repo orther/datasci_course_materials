f8e4768bbcd095a5b5f0e6c93556698150575429
Brandon Orther <coder@able.cd>
2014-07-11 12:34:40 -0700

    Added solution to assignment 1 problem 3.
    Also created a smaller output.txt file for faster processing.

--- assignment1/term_sentiment.py
+++ assignment1/term_sentiment.py
@@ -1,17 +1,31 @@
+import json
 import sys
 
-def hw():
-    print 'Hello, world!'
-
-def lines(fp):
-    print str(len(fp.readlines()))
+from tweet_sentiment import build_sentiment_scores
 
 def main():
-    sent_file = open(sys.argv[1])
-    tweet_file = open(sys.argv[2])
-    hw()
-    lines(sent_file)
-    lines(tweet_file)
+    sentiment_scores = build_sentiment_scores(sys.argv[1])
+    derived_scores   = dict()
+
+    with open(sys.argv[2]) as f:
+        for line in f:
+            # print line
+            new_terms = []
+            score     = 0
+            tweet     = json.loads(line)
+
+            for term in tweet.get("text", "").split():
+                if term in sentiment_scores:
+                    score += sentiment_scores.get(term, 0)
+
+                elif not term in new_terms:
+                    new_terms.append(term)
+
+            for term in new_terms:
+                derived_scores[term] = score + derived_scores.get(term, 0)
+
+    for term, score in derived_scores.iteritems():
+        print term, score
 
 if __name__ == '__main__':
     main()
